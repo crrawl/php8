3df319bfbaa964cbf7beaa481d74a3728ca7077e
yuukuramu <yuuhakobe1200@gmail.com>
2022-02-15 17:33:06 +0200

    ADD backend - login

--- login/login.php
+++ login/login.php
@@ -1,3 +1,62 @@
+<?php
+    
+    session_start();
+    if ($_SESSION) {
+        header("Location: index.php");
+    }
+    
+    include_once __DIR__ . "/functions.php";
+    include_once __DIR__ . "/database.php";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $name = secure($_POST["name"] ?? false);
+        $pwd = secure($_POST["passwd"] ?? false);
+        $scode = secure($_POST["scode"] ?? false);
+
+        $errors = [] ?? false;
+
+        if (!$name){ // if $name != true.
+            $errors["name"] = "This field is empty";
+        }
+        if (!$pwd){ // if $pwd != true.
+            $errors["password"] = "This field is empty";
+        }
+        if (!$scode){ // if $scode != true.
+            $errors["security_code"] = "This field is empty";
+        }
+        
+        if (!$errors) { // if $errors != true. 
+            $sql = "SELECT * FROM agents WHERE security_code = '$scode'";
+            $result = mysqli_query($conn, $sql);
+            $agent = mysqli_fetch_assoc($result);
+            
+            // Is agent security code finded in db
+            if (mysqli_num_rows($result) != 0) {
+                if ($name != $agent["agent"]) { 
+                    $errors["name"] = "Incorect agent name";
+                } else {
+                    $hash_verify = password_verify($pwd, $agent["password"]);
+                    
+                    if ($hash_verify) {
+                        $_SESSION["SESSID"] = session_create_id();
+                        $_SESSION["security_code"] = $scode;
+                        header("location: index.php");
+                    } else {
+                        $errors["password"] = "Incorect password";
+                    }
+                }
+            } else {
+                $errors["security_code"] = "Incorect security code";
+            }
+        }
+
+    }
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,15 +74,15 @@
     <form class="content" action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST">
         <div class="input-field">
             <label for="name">AGENT</label>
-            <input type="text">
+            <input type="text" name="name" placeholder="<?=$errors["name"] ?? ""?>">
         </div>
         <div class="input-field">
-        <label for="password">PASSWORD</label>
-            <input type="text">
+        <label for="passwd">PASSWORD</label>
+            <input type="password" name="passwd" placeholder="<?=$errors["password"] ?? ""?>">
         </div>
         <div class="input-field">
             <label for="scode">SECURITY CODE</label>
-            <input type="text">
+            <input type="text" name="scode" placeholder="<?=$errors["security_code"] ?? ""?>">
         </div>
 
         <button class="submit">
