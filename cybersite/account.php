8fcf1e1b316faccb862d5753bb9409415d82b1fc
yuukuramu <yuuhakobe1200@gmail.com>
2022-02-26 22:14:44 +0200

    redesign account.php

--- cybersite/account.php
+++ cybersite/account.php
@@ -15,27 +15,37 @@
 
 
 <section class="accounts">
+    <div class="header">
+        <h1>Accounts</h1>
+    </div>
     <?php if($result->num_rows > 0): ?>
-        <table class="user-accounts">
-            <tr>
-                <th>ID</th>
-                <th>UID</th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-            </tr>
-                <?php while($user = mysqli_fetch_assoc($result)):?>
-            <tr>
-                <td><?=$user["ID"];?></td>
-                <td><?=$user["uid"];?></td>
-                <td><?=$user["name"];?></td>
-                <td><?=$user["email"];?></td>
-                <td><a href="update.php?ID=<?=$user["ID"];?>">UPDATE</a></td>
-            </tr>
+        
+        <div class="user-accounts">
+            <!-- headers -->
+            <div class="account">ID</div>
+            <div class="account">UID</div>
+            <div class="account">NAME</div>
+            <div class="account">EMAIL</div>
+            <div class="account">UPDATE</div>
+            <div class="account">DELETE</div>
+
+            <!-- account list -->
+            <?php while($user = mysqli_fetch_assoc($result)):?>
+
+                <div class="account"><?=$user["ID"];?></div>
+                <div class="account"><?=$user["uid"];?></div>
+                <div class="account"><?=$user["name"];?></div>
+                <div class="account"><?=$user["email"];?></div>
+                <a href="update.php?ID=<?=$user["ID"];?>" class="account">UPDATE</a>
+                <a href="delete.php?ID=<?=$user["ID"];?>" class="account" style="background-color:#393939" >DELETE</a>
                 <?php endwhile;?>
-        </table>
-    <?php endif; ?>
+        </div>
+    <?php else: ?>
+        <h1>Nav atrastu kontu!!</h1>
+    <?php endif; ?> 
+
 </section>
 <div class="logout">
-    <h4>Hello, <?=$_SESSION["uid"]?></h4>
+    <h4 style="font-family: 'Ubuntu', sans-serif;">Hello, <?=$_SESSION["uid"]?></h4>
     <a href="logout.php">logout</a>
 </div>
